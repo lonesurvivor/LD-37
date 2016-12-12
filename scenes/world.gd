1e6864c6beff66b2d22c4fa494e70b00ec901e58
lonesurvivor <musicmylove@gmx.net>
2016-12-12 16:01:54 +0100

    trees

--- scenes/world.gd
+++ scenes/world.gd
@@ -44,7 +44,7 @@ func _ready():
 		
 	player_node = load("res://entities/player.tscn").instance()
 	next_scene_name = "present"
-	g.inventory_add(g.items.grappling_hook)
+	g.inventory_add(g.items.machete)
 	switch_scene_now()
 	yield(get_tree(), "idle_frame")
 	if(scene_node.has_node("spawn")):
